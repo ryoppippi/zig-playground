4bd4bbc43bd3828ea4ae44000323db049ef45252
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-01 21:31:57 +0100

    fix name

--- fizzbuzz/main_packed_structs.zig
+++ fizzbuzz/main_packed_structs.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 const allocPrint = std.fmt.allocPrint;
 
 fn fizzbuzz(i: u32, allocator: anytype) ![]const u8 {
-    const f = Flag{ .@"3" = @mod(i, 3) == 0, .@"5" = @mod(i, 5) == 0 };
-    return if (f.@"3") try allocPrint(allocator, "Fizz", .{}) else if (f.@"3" and f.@"5") try allocPrint(allocator, "FizzBuzz", .{}) else if (f.@"5") try allocPrint(allocator, "Buzz", .{}) else try allocPrint(allocator, "{d}", .{i});
+    const f = Flag{ .three = @mod(i, 3) == 0, .five = @mod(i, 5) == 0 };
+    return if (f.three) try allocPrint(allocator, "Fizz", .{}) else if (f.three and f.five) try allocPrint(allocator, "FizzBuzz", .{}) else if (f.five) try allocPrint(allocator, "Buzz", .{}) else try allocPrint(allocator, "{d}", .{i});
 }
 
 const Flag = packed struct {
-    @"3": bool = false,
-    @"5": bool = false,
+    three: bool = false,
+    five: bool = false,
 };
 
 pub fn main() anyerror!void {
